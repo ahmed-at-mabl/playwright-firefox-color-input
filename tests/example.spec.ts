1e548787715f630b193421911c21cc0d1eb97952
Ahmed Elbashir <ahmed@mabl.com>
2023-01-06 16:35:53 -0500

    Reduce test to just clicking on the input color element

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('enter text into a color picker element', async ({ page }) => {
-  const setColor = '#281cce';
   await page.goto(
     'https://storage.googleapis.com/public-dev-test-lab/input/inputtypes.html'
   );
   const inputColorElement = await page.$('#input-color');
   await inputColorElement?.click();
-  await page.fill('#input-color', setColor);
-  const outputColorElement = await page.$('#output-input-color');
-  expect(await outputColorElement?.innerText()).toEqual(setColor);
 });
