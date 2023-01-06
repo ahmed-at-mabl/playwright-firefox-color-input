c8a105911cdf7feb02aa8f5b729231be15d4b8c5
Ahmed Elbashir <ahmed@mabl.com>
2023-01-06 15:58:52 -0500

    Remove 2 example tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,22 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
-
 test('enter text into a color picker element', async ({ page }) => {
   const setColor = '#281cce';
   await page.goto(
